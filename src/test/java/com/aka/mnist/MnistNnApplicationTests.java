624c65345d26863cf0d90e9f2c9cf9bcb31b2447
AliaksandrKuchura <kuchura0409@gmail.com>
2020-08-28 17:23:03 +0300

    Spring initialization

---
+++ src/test/java/com/aka/mnist/MnistNnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aka.mnist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MnistNnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
