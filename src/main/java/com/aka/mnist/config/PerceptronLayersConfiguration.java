7f82bac72cfccb76fd40e56e72e92eac5c34de1b
AliaksandrKuchura <kuchura0409@gmail.com>
2020-08-30 20:31:09 +0300

    Added neuron network configuration

---
+++ src/main/java/com/aka/mnist/config/PerceptronLayersConfiguration.java
@@ -0,0 +1,38 @@
+package com.aka.mnist.config;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Aliaksandr Kuchura on Aug, 2020
+ */
+
+@Data
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "perceptron")
+public class PerceptronLayersConfiguration {
+
+    private String learningRate;
+
+    private List<Layer> layers = new ArrayList<>();
+
+    @Data
+    @Component
+    @ConfigurationProperties(prefix = "perceptron.layers")
+    private static class Layer {
+
+        private String name;
+
+        private Integer size;
+    }
+}
